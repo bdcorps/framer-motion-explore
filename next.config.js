90774809721de85f2145783ce958e7faac85738c
sukh <bdcorps@gmail.com>
2023-02-16 13:32:02 -0500

    add base path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true }
-    return config
-  },
-
-  assetPrefix: "/_micro-interactions",
+  basePath: '/micro-interactions',
 }
