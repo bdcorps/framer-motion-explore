a2d14d6d9a17e8f6d74c2f28433970a7209f7132
sukh <bdcorps@gmail.com>
2023-02-16 13:23:53 -0500

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import("next").NextConfig} */
 module.exports = {
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true }
     return config
   },
+
+  assetPrefix: "/_micro-interactions",
 }
